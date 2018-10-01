5b06ec26e8ecea9818a63ba0c69fce39ee8caa0c
timon0596 <timon0596@yandex.ru>
2018-10-02 00:48:22 +0300

    looped slider completed

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,50 @@
 var gal = document.getElementsByClassName("gallery")[0];
 var div = document.createElement('div');
+var div2 = document.createElement('div');
 var item1 = document.getElementsByClassName("item")[0];
+var item2 = document.getElementsByClassName("item");
+var itemlngt = item2.length;
+
 div.className = item1.className;
 div.innerHTML = item1.innerHTML;
 div.id = "firstClone";
 gal.insertBefore(div, gal.children[gal.children.length - 2]);
+div2.className = item1.className;
+div2.innerHTML = item2[itemlngt-1].innerHTML;
+
+div2.id = "lastClone";
+gal.insertBefore(div2, gal.children[0]);
 var items = document.getElementsByClassName("item");
 var itemWdt = parseFloat(getComputedStyle(item1).height);
 var hgt = document.getElementsByClassName("container")[0];
 var wdt;
 var lngt = items.length;
+
 var tr = item1.style.transition;
 
 function transFunc (lngt){
 	hgt.style.height = parseFloat(getComputedStyle(item1).height) 
 	+ "px";
 	wdt = parseFloat(getComputedStyle(item1).width);
-	for(let i=0;i<lngt;i++){
-		items[i].style.left = wdt*i + 'px';
+	items[0].style.left = -wdt + 'px';
+	for(let i=0;i<lngt-1;i++){
+		items[i+1].style.left = wdt*i + 'px';
 	}
+
+};
+function transFunc2 (lngt){
+	hgt.style.height = parseFloat(getComputedStyle(item1).height) 
+	+ "px";
+	wdt = parseFloat(getComputedStyle(item1).width);
+	
+	for(let i=0;i<lngt;i++){
+		items[i].style.left = wdt*i - (lngt-2)*wdt + 'px';
+	}
+	
 };
 transFunc(lngt);
 window.onresize = function (){transFunc(lngt);} 
-var galInnerHTML = gal.innerHTML;
+
 function next(){
 
 	for(let i of items){
@@ -42,6 +64,26 @@ document.getElementById("next").onclick = function(){
 	
 	
 	next();
+}
+function prev(){
+
+	for(let i of items){
+		i.style.transition = tr;
+		i.style.left = parseFloat(i.style.left) + wdt + 'px';
+		i.addEventListener('transitionend', function(){
+			for(let k of items){
+				if(k.id == 'lastClone' && k.style.left == '0px'){
+					for(let j of items){j.style.transition = 'none';}
+					transFunc2(lngt);
+				}
+			}
+		});
+	}
+};
+document.getElementById("prev").onclick = function(){
+	
+	
+	prev();
 }
 
 
